2f71636c8eeb20a870617f11108c579120c9f976
Hemen <boro.hemengips@gmail.com>
2019-06-16 00:55:02 +0530

    complete pre and 1 post(graduate group) visualization

--- SkateistanQuestionnaires.R
+++ SkateistanQuestionnaires.R
@@ -12,17 +12,21 @@ library(tidyverse)
 library(plyr)
 library(reshape2)
 library(wordcloud)
-library(tidytext)
-library(ggpubr)
-library(readxl)
-library(stringi)
-library("tm")
-library("SnowballC")
-library("wordcloud")
-library("RColorBrewer")
+library(tidytext)##
+library(ggpubr)## ballomnplot
+library(readxl)##reading excel
+library(stringi)#string operations
+library(tm)##wordcloud
+library(SnowballC)##wordcloud
+library(wordcloud)##wordcloud
+library(RColorBrewer)##wordcloud
+library(ggcorrplot)##correlogram plot
+library(qdapRegex) ##removing emotions
 
 
+### setting up directory
 
+setwd("D://personal/swb project/files_bts")
 
 
 ###loding files
@@ -30,6 +34,7 @@ library("RColorBrewer")
 ### pre questionnaire BTS 2017-18
 
 PreQ.PrevYear <- read_excel("Copy of Student Pre-Questionnaire Back-to-School 2017-2018 Group (Afghanistan).xlsx")
+
 
 ### cleaning the datasets:
 
@@ -89,7 +94,7 @@ PreQ.PrevYear$`I am good at reading and writing`[PreQ.PrevYear$`I am good at rea
 
 Freq_table_gender_location<- table(PreQ.PrevYear$Project_Site,PreQ.PrevYear$Gender)
 Freq_table_gender_location_df<- as.data.frame(Freq_table_gender_location)
-Freq_table_gender_location_df<- Freq_table_gender_location_df[c(-1,-2),]  ### freq table for gender based on location 
+Freq_table_gender_location_df<- Freq_table_gender_location_df[c(-1,-2),]### freq table for gender based on location 
 names(Freq_table_gender_location_df)[1:2]<-paste(c("Project_Sites","Gender"))  ### renaming the column names
 
 ### creating an barplot ---- freq count of gender in project_sites 
@@ -103,7 +108,7 @@ gender_count_barplot
 #extracting all role model columns with gender and project sites
 Freq_table_role_model<- PreQ.PrevYear %>% 
   dplyr:: select("Project_Site","Gender","Role Model - Father","Role Model - Brother/Sister","Role Model - Other family member","Role Model - Friends at Skateistan","Role Model - Skateistan Educator","Role Model - Someone older than me at Skateistan","Role Model - Other","Role Model - Would you like to say who?")
-Freq_table_role_model[Freq_table_role_model==""]<- "NULL"  # removing blanks with NA values
+Freq_table_role_model[Freq_table_role_model==""]<- "Not Available"  # removing blanks with NA values
 
 
 
@@ -125,9 +130,27 @@ role_model_freq_counts_reshape_table<-role_model_freq_counts_reshape%>%spread(Va
 
 rolemodel_freqcount_balloonplot<- ggballoonplot(role_model_freq_counts_reshape, fill = "value")+
   scale_fill_viridis_c(option = "C")
+rolemodel_freqcount_balloonplot
 
 
-## sentimental analysis
+### heatmap
+role_model_freq_counts_reshape_heatmap<-role_model_freq_counts_reshape%>%spread(L1,value)
+role_model_freq_counts_reshape_heatmap<- as.data.frame(role_model_freq_counts_reshape_heatmap)
+role_model_freq_counts_reshape_heatmap[is.na(role_model_freq_counts_reshape_heatmap)] <- 0
+role_model_freq_counts_reshape_heatmap2 <- as.matrix(role_model_freq_counts_reshape_heatmap[, -1])
+rownames(role_model_freq_counts_reshape_heatmap2) <- role_model_freq_counts_reshape_heatmap$Var1
+heatmap(role_model_freq_counts_reshape_heatmap2)
+
+### correlgram
+
+role_model_correltion<-role_model_freq_counts_reshape_heatmap[,-1]
+rownames(role_model_correltion)<-colnames(role_model_correltion)
+M<-round(cor(role_model_correltion),1)
+ggcorrplot(M,hc.order = TRUE,type='lower',lab=TRUE,lab_size=3, method="circle",colors = c("tomato2", "white", "springgreen3"), ggtheme=theme_bw,title="Correlogram of role models")
+
+
+
+#### sentimental analysis #####
 
 #extracting all the Role Model - Would you like to say who? rows 
 
@@ -140,6 +163,9 @@ role_model_who_says<-role_model_who_says[2]
 role_vec <- as.matrix(role_model_who_says)
 role_vec_spl_cha_remove<- gsub(".","",role_vec,fixed=TRUE)
 role_vec_spl_cha_remove<- as.vector(role_vec_spl_cha_remove)
+
+
+
 
 
 ## extract role ansers  columns
@@ -176,29 +202,84 @@ d<-d %>% filter(word !="null")
 p<-ggplot(d,aes(word,freq))+geom_bar(stat="identity")+geom_text(aes(label=freq))
 p<-p+theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-word_rolemodel<-wordcloud(words = d$word, freq = d$freq, min.freq = 1,
-          max.words=100, random.order=FALSE, rot.per=0.35, 
-          colors=brewer.pal(8, "Dark2"))
+word_rolemodel<-wordcloud(words = d$word, freq = d$freq, min.freq = 1,max.words=100, random.order=FALSE, rot.per=0.35, colors=brewer.pal(8, "Dark2"))
 word_rolemodel
 
 
-
+###################################################################################
+###################################################################################
 ###loding files
 
-### pre questionnaire BTS 2017-18
-
-PreQ.PrevYear <- read_excel("Copy of Student Pre-Questionnaire Back-to-School 2017-2018 Group (Afghanistan).xlsx")
+### pre questionnaire BTS 2018-19
 
 
-
+PreQ.CurrYear <- read_excel("Copy of Student Pre-Questionnaire Back-to-School 2018-2019 Group (Afghanistan).xlsx")
 
 
 
 
+PreQ.CurrYear <- tail(PreQ.CurrYear,-1)
 
 
 
- 
+
+require(dplyr)
+PreQ.CurrYear <- PreQ.CurrYear %>%
+  #as.Date(as.character(as.POSIXct("Start Date"),format="%Y-%m-%d")) %>%
+  #as.Date(as.character(as.POSIXct("End Date"),format="%Y-%m-%d")) %>%
+  replace(is.na(.), "") %>%
+  tidyr::unite("Project.Site", "Project Site","...11", sep = "", remove = TRUE) %>%
+  tidyr::unite("Gender", "What is your gender?", "...14","...15", sep = "", remove = TRUE) %>%
+  tidyr::unite("I believe I can work well in a team/group", "I believe I can......16", "...17","...18", sep = "", remove = TRUE) %>%
+  tidyr::unite("I believe I can speak in front of the class (public speaking)", "...19", "...20","...21", sep = "", remove = TRUE) %>%
+  tidyr::unite("I believe I can help other people", "...22", "...23","...24", sep = "", remove = TRUE) %>%
+  tidyr::unite("I believe I can ask for help when I have a problem", "...25", "...26","...27", sep = "", remove = TRUE) %>%
+  tidyr::unite("I believe I can make mistakes and learn from them", "...28", "...29","...30", sep = "", remove = TRUE) %>%
+  dplyr::rename("Examples (I believe section)" = "...31") %>%
+  tidyr::unite("How do you feel traveling to/from Skateistan", "How do you feel...", "...33","...34", sep = "", remove = TRUE) %>%
+  tidyr::unite("How do you feel in the skatepark", "...35", "...36","...37", sep = "", remove = TRUE) %>%
+  tidyr::unite("How do you feel in the classroom", "...38", "...39","...40", sep = "", remove = TRUE) %>%
+  tidyr::unite("How do you feel talking with an Educator at Skateistan", "...41", "...42","...43", sep = "", remove = TRUE) %>%
+  tidyr::unite("How do you feel learning with my class volunteer / youth leader", "...44", "...45","...46", sep = "", remove = TRUE) %>%
+  dplyr::rename("Examples (How do you feel section)" = "...47") %>%
+  tidyr::unite("In general I like myself", "In general I.", "...49","...50", sep = "", remove = TRUE) %>%
+  dplyr::rename("What makes you feel good/confident? What do you like about yourself?" = "...51") %>%
+  tidyr::unite("I believe I can spend time studying", "I believe I can......52", "...53","...54", sep = "", remove = TRUE) %>%
+  tidyr::unite("I believe I can do well in school", "...55", "...56","...57", sep = "", remove = TRUE) %>%
+  tidyr::unite("I believe I can enjoy sport/skateboarding skills", "...58", "...59","...60", sep = "", remove = TRUE) %>%
+  tidyr::unite("I believe I can learn new skateboard tricks", "...61", "...62","...63", sep = "", remove = TRUE) %>%
+  dplyr::rename("Examples (I believe I can section)" = "...64") %>%
+  tidyr::unite("I am good at reading", "I am good at....", "...66","...67", sep = "", remove = TRUE) %>%
+  tidyr::unite("I am good at writing", "...68", "...69","...70", sep = "", remove = TRUE) %>%
+  tidyr::unite("I am good at math", "...71", "...72","...73", sep = "", remove = TRUE) %>%
+  tidyr::unite("I am good at skateboarding", "...74", "...75","...76", sep = "", remove = TRUE) %>%
+  tidyr::unite("I am good at other sports", "...77", "...78","...79", sep = "", remove = TRUE) %>%
+  dplyr::rename("Examples (I am good at section)" = "...80") %>%
+  tidyr::unite("Over the past 2-3 weeks I have felt happy", "Over the past 2-3 weeks...", "...82","...83", sep = "", remove = TRUE) %>%
+  tidyr::unite("Over the past 2-3 weeks I have felt calm and relaxed", "...84", "...85","...86", sep = "", remove = TRUE) %>%
+  tidyr::unite("Over the past 2-3 weeks I wake up feeling fresh and rested", "...87", "...88","...89", sep = "", remove = TRUE) %>%
+  tidyr::unite("Over the past 2-3 weeks My daily life has been filled with things that interest me", "...90", "...91","...92", sep = "", remove = TRUE) %>%
+  dplyr::rename("Role Model - Mother" = "When I have problems or I am upset, I can talk to... (you can choose more than one) (Role Model)") %>%
+  dplyr::rename("Role Model - Father" = "...94") %>%
+  dplyr::rename("Role Model - Brother" = "...95") %>%
+  dplyr::rename("Role Model - Sister" = "...96") %>%
+  dplyr::rename("Role Model - Other family member" = "...97") %>%
+  dplyr::rename("Role Model - Friends at Skateistan" = "...98") %>%
+  dplyr::rename("Role Model - Skateistan Educator" = "...99") %>%
+  dplyr::rename("Role Model - Someone older than me at Skateistan" = "...100") %>%
+  dplyr::rename("Role Model - Other" = "...101") %>%
+  dplyr::rename("Role Model - Not sure" = "...102") %>%
+  dplyr::rename("Role Model - Would you like to say who? How do you feel when you talk to them about your problems?" = "...103") %>%
+  tidyr::unite("My parents/family want me to complete my education until", "My parents/family want me to complete my education until...", "...105","...106", "...107", "...108", sep = "", remove = TRUE) %>%
+  dplyr::rename("Education level - Why/why not?" = "...109")
+
+### removing smiley
+PreQ.CurrYear[] <- lapply(PreQ.CurrYear, rm_emoticon)
+
+
+
+
+
  
  
 
